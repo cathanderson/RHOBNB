afe3d914f5f7cd1597088fe145fe31e4bdb1ba6e
“Catherine <cathanders10@gmail.com>
2022-12-03 15:15:35 -0500

    application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,36 @@ class ApplicationController < ActionController::API
     before_action :snake_case_params
     before_action :attach_authenticity_token
 
+    def current_user 
+        @current_user ||= User.find_by(session_token: session[:session_token])
+    end
+
+    def require_logged_in
+        if !logged_in?
+            render json: { errors: ['Must be logged in'] }, status: :unauthorized
+        end
+    end
+
+    def require_logged_out
+        if logged_in?
+            render json: { errors: ['Must be logged out'] }, status: 403
+        end
+    end
+
+    def logged_in?
+        !!current_user
+    end
+
+    def login(user)
+        session[:session_token] = user.reset_session_token!
+    end
+
+    def logout
+        current_user.reset_session_token!
+        session[:session_token] = nil
+        @current_user = nil
+    end
+
     private
 
     def snake_case_params
