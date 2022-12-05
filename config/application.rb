9583355c26d829c28d5e43518e24193b4911914c
“Catherine <cathanders10@gmail.com>
2022-12-05 16:25:08 -0500

    prep for inital push to render

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,9 @@ module RHOBNB
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+
+    #below middleware stuff
+    config.railties_order = [:all, :main_app]
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+
+
+  # make sure this is below other rotes
+  get '*path', to: "static_pages#frontend_index"
 end
