5bf6bef523ab525184269bab877484033003ef05
“Catherine <cathanders10@gmail.com>
2022-12-07 10:01:39 -0500

    fetchProperty and addProperty done, proprtiesReducer tweaked

--- frontend/src/store/properties.js
+++ frontend/src/store/properties.js
@@ -1,10 +1,16 @@
 import csrfFetch from "./csrf";
 
 const SET_PROPERTIES = "properties/setProperties";
+const ADD_PROPERTY = "properties/addProperty";
 
 const setProperties = (properties) => ({
   type: SET_PROPERTIES,
   payload: properties,
+});
+
+export const addProperty = (property) => ({
+  type: ADD_PROPERTY,
+  payload: property,
 });
 
 export const fetchProperties = () => async (dispatch) => {
@@ -12,15 +18,26 @@ export const fetchProperties = () => async (dispatch) => {
   const data = await response.json();
   dispatch(setProperties(data.properties));
   //   return data;
+  return response;
+};
+
+export const fetchProperty = (propertyId) => async (dispatch) => {
+  const response = await csrfFetch(`/api/properties/${propertyId}`);
+  const data = await response.json();
+//   console.log(data)
+    // debugger
+  dispatch(addProperty(data));
+//   return response;
 };
 
 function propertiesReducer(state = {}, action) {
   switch (action.type) {
     case SET_PROPERTIES:
       return action.payload;
-    // case ADD_BENCH:
-    //   const bench = action.payload;
-    //   return { ...state, [bench.id]: bench };
+    case ADD_PROPERTY:
+      const property = action.payload;
+    //   debugger
+      return { ...state, [property.id]: property };
     default:
       return state;
   }
