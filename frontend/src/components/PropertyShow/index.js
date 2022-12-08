7329a5119c794516d15919939ed6bc1a76b23dff
“Catherine <cathanders10@gmail.com>
2022-12-08 16:34:01 -0500

    started styling show page

--- frontend/src/components/PropertyShow/index.js
+++ frontend/src/components/PropertyShow/index.js
@@ -59,7 +59,38 @@ function PropertyShowPage() {
           <li className="show-page-city-state-us">
             {city}, {state}, United States
           </li>
-          <li className="show-page-hosted-by">{propertyType} hosted by {hostName}</li>
+          <li>
+            <div className="property-show-image-container">
+              <div className="main-property-show-image-container">
+                <img
+                  className="main-property-show-image"
+                  src={property.photoUrls[0]}
+                ></img>
+              </div>
+              <div className="non-main-property-show-image-container">
+                <img
+                  className="non-main-property-show-image"
+                  src={property.photoUrls[1]}
+                ></img>
+                <img
+                  className="non-main-property-show-image"
+                  src={property.photoUrls[2]}
+                ></img>
+                <img
+                  className="non-main-property-show-image"
+                  src={property.photoUrls[3]}
+                ></img>
+                <img
+                  className="non-main-property-show-image"
+                  src={property.photoUrls[4]}
+                ></img>
+              </div>
+            </div>
+          </li>
+          <li></li>
+          <li className="show-page-hosted-by">
+            {propertyType} hosted by {hostName}
+          </li>
           <li className="show-page-room-details-container">
             <li className="show-page-num-beds-num-baths">
               {bedDetails} · {bathDetails}
