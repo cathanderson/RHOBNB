e48f147db6293c806b09014a8a3dd1ff8b9cf421
“Catherine <cathanders10@gmail.com>
2022-12-13 12:18:45 -0500

    category icons looking cute

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -6,6 +6,7 @@ import "./Navigation.css";
 import LoginFormModal from "../LoginFormModal";
 import NavSearch from "./NavSearch.js";
 import { Link } from "react-router-dom";
+import dress from "../../assets/images/dress.png"
 
 function Navigation() {
   const sessionUser = useSelector((state) => state.session.user);
@@ -21,7 +22,10 @@ function Navigation() {
     <>
       <nav className="main-nav">
         <Link id="app-title-link" to="/">
-          <h1 className="app-title">rhobnb</h1>
+          <h1 className="app-title">
+            <img id="app-logo" src={dress}/>
+            rhobnb
+            </h1>
         </Link>
         <NavSearch />
         {sessionNav}
--- frontend/src/components/PropertiesIndex/index.js
+++ frontend/src/components/PropertiesIndex/index.js
@@ -2,7 +2,13 @@ import { useSelector, useDispatch } from "react-redux";
 import { fetchProperties } from "../../store/properties";
 import PropertiesList from "./PropertiesList";
 import { useEffect } from "react";
-import "./PropertiesIndex.css"
+import "./PropertiesIndex.css";
+import tv from "../../assets/category-icons/tv.png";
+import apple from "../../assets/category-icons/apple.png";
+import pizza from "../../assets/category-icons/pizza-slice.png";
+import snowflake from "../../assets/category-icons/snowflake.png";
+import peach from "../../assets/category-icons/peach.png";
+
 
 function PropertiesIndex() {
   const properties = useSelector((state) => Object.values(state.properties));
@@ -14,9 +20,33 @@ function PropertiesIndex() {
 
   return (
     <>
-        <PropertiesList properties = {properties}/>
+      <div className="categories-icons-container">
+        <div className="category-icon-container">
+          <img className="category-icon" src={tv} />
+          <label className="category-icon-label">All properties</label>
+        </div>
+        <div className="category-icon-container">
+          <img className="category-icon" src={apple} />
+          <label className="category-icon-label">New York</label>
+        </div>
+        <div className="category-icon-container">
+          <img className="category-icon" src={pizza} />
+          <label className="category-icon-label">New Jersey</label>
+        </div>
+        <div className="category-icon-container">
+          <img className="category-icon" src={snowflake} />
+          <label className="category-icon-label">Salt Lake City</label>
+        </div>
+        <div className="category-icon-container">
+          <img className="category-icon" src={peach} />
+          <label id="category-icon-label-atlanta" className="category-icon-label">
+            Atlanta
+          </label>
+        </div>
+      </div>
+      <PropertiesList properties={properties} />
     </>
-  )
+  );
 }
 
 export default PropertiesIndex;
