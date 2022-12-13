a0ee783bf83f2b7d0b886feaf744a410c3449953
“Catherine <cathanders10@gmail.com>
2022-12-13 16:54:40 -0500

    able to show google map using string api key

--- frontend/src/components/PropertiesIndex/index.js
+++ frontend/src/components/PropertiesIndex/index.js
@@ -1,10 +1,11 @@
 import { useSelector, useDispatch } from "react-redux";
 import { fetchProperties, fetchPropertiesByFranchise } from "../../store/properties";
 import PropertiesList from "./PropertiesList";
-import { useEffect } from "react";
+import { React, useEffect } from "react";
 import "./PropertiesIndex.css";
 import PropertyFranchiseBar from "../PropertyFranchiseBar";
 import { useParams } from "react-router-dom";
+import MapContainer from "../MapContainer";
 
 
 function PropertiesIndex() {
@@ -20,12 +21,23 @@ function PropertiesIndex() {
     }
   }, [dispatch, rh_franchise]);
 
-  return (
-    <>
-      <PropertyFranchiseBar/>
-      <PropertiesList properties={properties} />
-    </>
-  );
+  if (!rh_franchise) {
+    return (
+      <>
+        <PropertyFranchiseBar />
+        <PropertiesList properties={properties} />
+      </>
+    );
+  } else {
+    debugger
+    return (
+      <>
+        <PropertyFranchiseBar />
+        <PropertiesList properties={properties} />
+        <MapContainer/>
+      </>
+    );
+  }
 }
 
 export default PropertiesIndex;
