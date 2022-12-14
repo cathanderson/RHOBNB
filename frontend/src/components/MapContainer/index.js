73bde51bf0256533b40f98b8ec3d24e6fc65da95
“Catherine <cathanders10@gmail.com>
2022-12-14 09:41:40 -0500

    got map api key stored securely

--- frontend/src/components/MapContainer/index.js
+++ frontend/src/components/MapContainer/index.js
@@ -1,30 +1,31 @@
 import React from "react";
-import { GoogleMap, LoadScript } from '@react-google-maps/api';
+import { GoogleMap, LoadScript } from "@react-google-maps/api";
+// import REACT_APP_MAPS_API_KEY from "../../../env.development.local"
 
 const MapContainer = () => {
-  
-  const mapStyles = {        
+  const mapStyles = {
     height: "100vh",
     width: "100%",
   };
-  
+
   const defaultCenter = {
-    lat: 41.3851, lng: 2.1734
-  }
-  
+    lat: 41.3851,
+    lng: 2.1734,
+  };
+
   return (
     // <section className="map-container">
-      <LoadScript
-        // googleMapsApiKey={process.env.REACT_APP_MAPS_API_KEY}
-      >
-        <GoogleMap
-          mapContainerStyle={mapStyles}
-          zoom={13}
-          center={defaultCenter}
-        />
-      </LoadScript>
+    <LoadScript
+      googleMapsApiKey={process.env.REACT_APP_MAPS_API_KEY}
+    >
+      <GoogleMap
+        mapContainerStyle={mapStyles}
+        zoom={13}
+        center={defaultCenter}
+      />
+    </LoadScript>
     // </section>
   );
-}
+};
 
 export default MapContainer;
