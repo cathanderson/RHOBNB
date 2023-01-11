9e58945b12f48720e1081072264f5e60d79784b2
“Catherine <cathanders10@gmail.com>
2023-01-11 17:56:16 -0500

    specify search coming soon

--- frontend/src/components/Navigation/NavSearch.js
+++ frontend/src/components/Navigation/NavSearch.js
@@ -7,7 +7,7 @@ function NavSearch() {
         <div className="searchbar-fields">
           <div className="searchbar-anywhere">Anywhere</div>
           <div className="searchbar-any-wife">Any wife</div>
-          <div className="searchbar-add-guests">Add guests</div>
+          <div className="searchbar-add-guests">Search coming soon!</div>
         </div>
         <i className="fa-solid fa-magnifying-glass"></i>
       </div>
